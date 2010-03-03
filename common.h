4e8b2ac95c6809c511045909ec07a6b2fd0dd304
Yang Hamo Bai <hamo.by@gmail.com>
2010-03-03 22:42:01 +0800

    add common.h

---
+++ common.h
@@ -0,0 +1,16 @@
+/*
+ *
+ * Copyright (C) 2010 Yang Hamo Bai < hamo.by@gmail.com >
+ *
+ * This code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#ifndef LOGIC
+#define LOGIC
+#define SUCCESS 0
+#define ERR -1
+#define OK 0
+#endif
